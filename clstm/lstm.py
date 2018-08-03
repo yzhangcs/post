fix order error & memory overflow

clstm/lstm.py
@@ -13,44 +13,36 @@ from torch.nn.utils.rnn import (pack_padded_sequence, pad_packed_sequence,
 
 class WordLSTM(nn.Module):
 
-    def __init__(self, vocdim, chrdim, embdim, hiddim, outdim, embed):
+    def __init__(self, vocdim, chrdim, wembdim, cembdim, hiddim, embed):
         super(WordLSTM, self).__init__()
 
         # 词汇维度
         self.vocdim = vocdim
-        # 字向量维度
-        self.embdim = embdim
-        # 隐藏层维度
+        # 词向量维度
+        self.wembdim = wembdim
+        # 隐藏态维度
         self.hiddim = hiddim
-        # 输出层维度
-        self.outdim = outdim
         # 词嵌入
         self.embed = nn.Embedding.from_pretrained(embed, False)
         # 词嵌入LSTM层
-        self.wlstm = nn.LSTM(embdim + embdim, self.hiddim, batch_first=True)
+        self.wlstm = nn.LSTM(wembdim + cembdim, self.hiddim, batch_first=True)
         # 字嵌入LSTM层
-        self.clstm = CharLSTM(chrdim, embdim, embdim)
+        self.clstm = CharLSTM(chrdim, cembdim, cembdim)
 
     def forward(self, wx, cx, wlens, clens):
         B, T = wx.shape
-
         # 获取词嵌入向量
-        wx = self.embed(wx).view(B, T, -1)
+        wx = self.embed(wx)
         # 获取字嵌入向量
-        cx = pad_sequence([
-            self.clstm(cx[i], clens[i]) for i, wl in enumerate(wlens)
-        ], batch_first=True)
+        cx = self.clstm(cx.view(B * T, -1), clens.view(-1))
+        cx = cx.view(B, T, -1)
 
+        # 拼接词表示和字表示
         x = torch.cat((wx, cx), dim=-1)
         x = pack_padded_sequence(x, wlens, batch_first=True)
 
-        hidden = self.init_hidden(B)
-        x, hidden = self.wlstm(x, hidden)
+        x, hidden = self.wlstm(x)
         return x.data
-
-    def init_hidden(self, batch_size):
-        return (nn.init.orthogonal_(torch.zeros(1, batch_size, self.hiddim)),
-                nn.init.orthogonal_(torch.zeros(1, batch_size, self.hiddim)))
 
 
 class CharLSTM(nn.Module):
@@ -76,16 +68,13 @@ class CharLSTM(nn.Module):
         # 获取长度由大到小排列的字序列索引
         lens, indices = lens.sort(descending=True)
         # 获取反向索引用来恢复原有的顺序
-        reversed_indices = indices.sort(descending=True)[1]
+        _, reversed_indices = indices.sort()
+        # 获取单词最大长度
+        maxlen = lens[0]
         # 序列按长度由大到小排列
-        x = x.view(B, T, -1)[indices]
-
+        x = x.view(B, T, -1)[indices, :maxlen]
         x = pack_padded_sequence(x, lens, batch_first=True)
-        hidden = self.init_hidden(B)
 
-        x, hidden = self.lstm(x, hidden)
+        x, hidden = self.lstm(x)
+        # 返回词的字符表示
         return hidden[0].squeeze()[reversed_indices]
-
-    def init_hidden(self, batch_size):
-        return (nn.init.orthogonal_(torch.zeros(1, batch_size, self.hiddim)),
-                nn.init.orthogonal_(torch.zeros(1, batch_size, self.hiddim)))

clstm/network.py
@@ -15,15 +15,17 @@ from lstm import WordLSTM
 
 class Network(nn.Module):
 
-    def __init__(self, vocdim, chrdim, embdim,
+    def __init__(self, vocdim, chrdim,
+                 wembdim, cembdim,
                  hiddim, outdim,
                  lossfn, embed):
         super(Network, self).__init__()
 
         # LSTM层
-        self.lstm = WordLSTM(vocdim, chrdim, embdim, hiddim, outdim, embed)
+        self.lstm = WordLSTM(vocdim, chrdim, wembdim, cembdim, hiddim, embed)
         # 输出层
         self.out = nn.Linear(hiddim, outdim)
+
         self.dropout = nn.Dropout()
         self.lossfn = lossfn
 
@@ -55,9 +57,10 @@ class Network(nn.Module):
                 optimizer.zero_grad()
                 # 获取长度由大到小排列的词序列索引
                 wlens, indices = wlens.sort(descending=True)
+                maxlen = wlens[0]
                 # 调整序列顺序
-                wx = wx[indices]
-                cx, clens, y = cx[indices], clens[indices], y[indices]
+                cx, clens = cx[indices, :maxlen], clens[indices, :maxlen]
+                wx, y = wx[indices, :maxlen], y[indices, :maxlen]
 
                 output = self(wx, cx, wlens, clens)
                 y = pack_padded_sequence(y, wlens, True).data
@@ -87,17 +90,21 @@ class Network(nn.Module):
         print(f"max accuracy of dev is {max_accuracy:.2%} at epoch {max_e}")
         print(f"mean time of each epoch is {total_time / (epoch + 1)}s\n")
 
-    def evaluate(self, data):
+    def evaluate(self, data, batch_size=25):
         # 设置为评价模式
         self.eval()
 
         loss, tp, total = 0, 0, 0
-        wx, cx, wlens, clens, y = data
-        output = self(wx, cx, wlens, clens)
-        y = pack_padded_sequence(y, wlens, True).data
-        loss = self.lossfn(output, y)
-        tp = (torch.argmax(output, dim=1) == y).sum().item()
-        total = wlens.sum().item()
+        dataset = TensorDataset(*data)
+        train_loader = DataLoader(dataset=dataset,
+                                  batch_size=batch_size)
+        with torch.no_grad():
+            for wx, cx, wlens, clens, y in train_loader:
+                output = self(wx, cx, wlens, clens)
+                y = pack_padded_sequence(y, wlens, True).data
+                loss += self.lossfn(output, y, size_average=True)
+                tp += (torch.argmax(output, dim=1) == y).sum().item()
+                total += wlens.sum().item()
         return loss, tp, total, tp / total
 
     def dump(self, file):